f2016eb5161511cbc6a07f566c98503378ea8884
Bethany Garcia <bethanygarcia817@gmail.com>
2018-11-27 14:42:45 +0000

    RPS GAME

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-`// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-//GLOBAL VARIABLES
-/* global $ */
+var userChoice = ""
+var computerChoice = ""
+var winner = ""
+var randomNumber = 0;
 
-
-
-// DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+    randomNumber=Math.random();
+    userChoice=$("#userChoice").val();
+    if(randomNumber < .33) {
+        computerChoice="rock"; 
+    }else if(randomNumber > .33 && randomNumber < .66) {
+        computerChoice="Paper!";
+    }else if(randomNumber > .66) {
+        computerChoice="Scissors!";
+    }
+    console.log(randomNumber);
+    console.log(computerChoice);
+});
 

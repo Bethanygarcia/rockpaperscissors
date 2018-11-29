6fd75ed675edd8e9523e0d2a7ad63dfe8ff2582a
Bethany Garcia <bethanygarcia817@gmail.com>
2018-11-29 01:41:42 +0000

    RPS Game 11/28/18

--- js/script.js
+++ js/script.js
@@ -6,15 +6,47 @@ var randomNumber = 0;
 
 $("#shoot").click(function(){
     randomNumber=Math.random();
-    userChoice=$("#userChoice").val();
+    userChoice = $("#input").val();
+    $("#userChoice").text(userChoice);     
+    
     if(randomNumber < .33) {
         computerChoice="rock"; 
+        $("#computerChoice").html("rock")
     }else if(randomNumber > .33 && randomNumber < .66) {
-        computerChoice="Paper!";
+        computerChoice="paper";
+        $("#computerChoice").html("paper")
     }else if(randomNumber > .66) {
-        computerChoice="Scissors!";
+        computerChoice="scissors";
+        $("#computerChoice").html("scissors")
+    }
+    if(computerChoice==="rock" && userChoice==="rock"){
+            $("#result1").html("Its is a draw");
+            
+        }else if(computerChoice==="rock" && userChoice==="paper"){
+            $("#result1").html("User Wins! Do you want a cookie?");
+        }else if(computerChoice==="rock" && userChoice==="scissors"){
+            $("#result1").html("Wow, you let a computer win? FAILURE!");
+    }
+    if(computerChoice==="paper" && userChoice==="paper"){
+        $("#result1").html("Its is a draw");
+        }else if(computerChoice==="paper" && userChoice==="scissors"){
+            $("#result1").html("User Wins! Do you want a cookie?");
+        }else if(computerChoice==="paper" && userChoice==="rock"){
+            $("#result1").html("Wow, you lost to a computer? FAILURE!");
+    }
+    if(computerChoice==="scissors" && userChoice==="scissors"){
+        $("#result1").html("Its is a draw");
+        }else if(computerChoice==="scissors" && userChoice==="rock"){
+            $("#result1").html("User Wins! Do you want a cookie?");
+        }else if(computerChoice==="scissors" && userChoice==="paper"){
+            $("#result1").html("Wow, you lost to a computer? FAILURE!");
     }
     console.log(randomNumber);
     console.log(computerChoice);
+$("#play").click(function(){
+   location.reload(); 
+   $("#play").hide();
+});
+    
 });
 

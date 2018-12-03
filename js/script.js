4b3741df7377624d8dfd3a265d85793ce8b60c73
Bethany Garcia <bethanygarcia817@gmail.com>
2018-12-03 03:54:54 +0000

    RPS Game Current

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $("#shoot").click(function(){
         $("#computerChoice").html("scissors")
     }
     if(computerChoice==="rock" && userChoice==="rock"){
-            $("#result1").html("Its is a draw");
+            $("#result1").html("Its a draw");
             
         }else if(computerChoice==="rock" && userChoice==="paper"){
             $("#result1").html("User Wins! Do you want a cookie?");
@@ -28,14 +28,14 @@ $("#shoot").click(function(){
             $("#result1").html("Wow, you let a computer win? FAILURE!");
     }
     if(computerChoice==="paper" && userChoice==="paper"){
-        $("#result1").html("Its is a draw");
+        $("#result1").html("Its a draw");
         }else if(computerChoice==="paper" && userChoice==="scissors"){
             $("#result1").html("User Wins! Do you want a cookie?");
         }else if(computerChoice==="paper" && userChoice==="rock"){
             $("#result1").html("Wow, you lost to a computer? FAILURE!");
     }
     if(computerChoice==="scissors" && userChoice==="scissors"){
-        $("#result1").html("Its is a draw");
+        $("#result1").html("Its a draw");
         }else if(computerChoice==="scissors" && userChoice==="rock"){
             $("#result1").html("User Wins! Do you want a cookie?");
         }else if(computerChoice==="scissors" && userChoice==="paper"){
